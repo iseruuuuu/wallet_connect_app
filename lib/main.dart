cbd089595055ace54cca9fe95c624b2495529e1f
iseruuuuu <iseki.ryutaro@rkb.bb4u.ne.jp>
2024-03-20 13:42:31 +0900

    add: wallet connectの実装をしました🪛

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:wallet_connect_app/constants/secret_key.dart';
+import 'package:web3modal_flutter/web3modal_flutter.dart';
 
 void main() {
   runApp(const MyApp());
@@ -27,16 +29,43 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  late W3MService _w3mService;
 
+  @override
+  void initState() {
+    super.initState();
+    loadWalletConnect();
+  }
+
+  Future<void> loadWalletConnect() async {
+    _w3mService = W3MService(
+      projectId: projectID,
+      metadata: const PairingMetadata(
+        name: 'Web3Modal Flutter Example',
+        description: 'Web3Modal Flutter Example',
+        url: 'https://www.walletconnect.com/',
+        icons: ['https://walletconnect.com/walletconnect-logo.png'],
+        redirect: Redirect(
+          native: 'flutterdapp://', // your own custom scheme
+          universal: 'https://www.walletconnect.com',
+        ),
+      ),
+    );
+    await _w3mService.init();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
+            const Text('You can tell the button below to connect the wallet'),
+            W3MConnectWalletButton(service: _w3mService),
+            W3MNetworkSelectButton(service: _w3mService),
+            W3MAccountButton(service: _w3mService),
           ],
         ),
       ),
